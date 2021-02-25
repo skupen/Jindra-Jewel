e976ec8c39326877da27d377f6cb3abb3ae19506
skupen <sv4b@hotmail.com>
2021-02-25 23:41:15 +0100

    Axios and Fixes
    Experimenting with axios and delete button fix.

--- edit.js
+++ edit.js
@@ -1,9 +1,45 @@
+// const getUsers = () => {
+//     axios.get('https://reqres.in/api/users')
+//     .then(response => {
+//      const users = response.data.data;
+//      console.log(`GET users`, users);
+//    })
+//     .catch(error => console.error(error));
+//    };
+//    getUsers();
+
+//const { default: axios } = require("axios");
+
+function getEls() {
+    axios.get("http://dev.contactifyapp.eu:9110/api/jewel/component/lol")
+    .then(response => {
+        console.log(response.data);
+    })
+}
+
+function postULs(id, li) {
+    axios.post(
+        "http://dev.contactifyapp.eu:9110/api/jewel/component/lol", 
+        {data: JSON.stringify({ id: id, content: li})}
+    )
+    .then(response => {
+        console.log(response.data);
+    })
+}
+
 // Helper
 function setAttributes(el, attrs) {
     for(var key in attrs) {
       el.setAttribute(key, attrs[key]);
     }
   }
+
+function ID() {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal.
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
 
 let editableElements = document.querySelectorAll("[data-jewel]");
 
@@ -19,14 +55,15 @@ function dataJewelInit(els) {
         setAttributes(element, {
             "x-data": "edit()",
             "data-active": "false",
-            "x-on:click": "{$component('panelRef').openSettingsPanel(), elToEdit()}"
+            "x-on:click": "{$component('panelRef').openSettingsPanel(), elToEdit()}",
         })
+        
                 
         if(oSettings.allowAdd === true){
             setAttributes(element, {
                 "x-data": "add()",
                 "x-on:click": "",
-                "x-ref": "ulRef"
+                "x-ref": "ulRef",
             })
     
             if(!element.getAttribute("data-addButtonExists")){
@@ -111,6 +148,7 @@ function panel() {
             for (let index = 0; index < editableElements.length; index++) {
                 if (editableElements[index].getAttribute("data-active") == "true") {
                     editableElements[index].innerHTML = this.$refs.panelInput.value;
+                    //postEls(editableElements[index].getAttribute("data-id"), this.$refs.panelInput.value);
                     editableElements[index].setAttribute("data-active", "false");
                 }       
             }
@@ -126,3 +164,19 @@ function panel() {
         }
     }
 }
+
+/*
+UL v html bude mit ID natvrdo a bude empty krome template
+
+na serveru bude schovany ID UL a k tomu vsechny LI
+
+oninit GET IDs vsech UL a jejich LI
+
+matchnout ID z GETu s ID z html
+
+render LI do prislusneho UL
+
+Pri kazdym pridani noveho itemu se POSTne updately LIs z UL s prislusnym ID
+
+
+*/
